ea9bf668d374d3f01b730c208ffb314e26110a8d
Sourabh Pandit <spandit@lanl.gov>
2026-01-16 11:48:17 -0700

    clean up

--- run_mpp_nsteps_03_modelsize_b.bash
+++
@@ -1,6 +0,0 @@
-python train_basic.py                        \
-       --run_name train_MPP_nsteps_03_avit_b \
-       --config basic_config                 \
-       --yaml_config config/config_MPP/mpp_avit_b_config_nsteps_03.yaml \
-       &>> ./mpp-output/MPP/train_MPP/out_train_MPP_nsteps_03_avit_b.txt
-
