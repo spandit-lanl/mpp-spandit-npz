9a3c4e064813f7e7571186af302fe542474d76c5
Sourabh Pandit <spandit@lanl.gov>
2026-01-16 18:15:27 -0700

    update sp-utils files

--- sp-utils/plots/00_run_all.bash
+++ sp-utils/plots/00_run_all.bash
@@ -9,11 +9,11 @@ python 03_verify_combines_losses.py
 python 04_plot_grid_losses.py --phase finetune --which all --output finetune_all_grid.png
 python 04_plot_grid_losses.py --phase pretrain --which all --output pretrain_all_grid.png
 
-python 04_plot_grid_losses.py --phase finetune --which odd --output finetune_odd_grid.png
-python 04_plot_grid_losses.py --phase pretrain --which odd --output pretrain_odd_grid.png
+#python 04_plot_grid_losses.py --phase finetune --which odd --output finetune_odd_grid.png
+#python 04_plot_grid_losses.py --phase pretrain --which odd --output pretrain_odd_grid.png
 
-python 04_plot_grid_losses.py --phase pretrain --which even --output pretrain_even_grid.png
-python 04_plot_grid_losses.py --phase finetune --which even --output finetune_even_grid.png
+#python 04_plot_grid_losses.py --phase pretrain --which even --output pretrain_even_grid.png
+#python 04_plot_grid_losses.py --phase finetune --which even --output finetune_even_grid.png
 
 #python 05_updated_plot_grid_losses_range.py --phase finetune --which all --nstep_min 1 --nstep_max 8  --output finetune_1to8_grid.png
 #python 05_updated_plot_grid_losses_range.py --phase finetune --which odd --nstep_min 1 --nstep_max 8  --output finetune_odd_1to8_grid.png
